aefa2727c3f651ffe6ab52a68e2d00fef09244fc
Juan Paolo R. Ibarra <jpibarra1130@gmail.com>
2011-02-03 11:09:31 +0800

    Initial commit for RoR demo app.
    Signed-off-by: Juan Paolo R. Ibarra <jpibarra1130@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1a4319df40acdefa4222d3e8d812f436e94c4f219750bd252b582c6ddf4ffe3e0bbe26260a0cc34cbff7b23f4c8285e5b09707563fc6b4e5aa6e3f67e948731e'
